6e15d7e0851fd05dd3007b104476f0cbedd52a94
mikehansonn <90873556+mikehansonn@users.noreply.github.com>
2025-05-23 16:43:25 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+import os
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import logging
+from api import bots
+
+# Run application with
+# uvicorn main:app --reload
+# runs on http://localhost:8000
+
+app = FastAPI()
+
+# Configure CORS
+origins = [
+    "http://localhost:3000" # also put the link to the webpage here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # allow all origins, form all the different bots
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Active"}
+
+app.include_router(bots.router, tags=["bots"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
